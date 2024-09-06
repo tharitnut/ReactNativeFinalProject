d7bb0e9ff5280b09bd761d1b98c01546eeb8ef09
tharitnut <nong.peanut.butter.mail@gmail.com>
2024-09-06 21:07:56 +0700

    Add Phase 2

--- ActiveAlert-App/App.tsx
+++ ActiveAlert-App/App.tsx
@@ -1,20 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text } from "react-native";
+import React from "react";
 
-export default function App() {
+import HomeScreen from "./screens/HomeScreen";
+
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+function App(): React.JSX.Element {
+  const Homestack = createNativeStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Homestack.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          //Global
+          headerStyle: { backgroundColor: "#20b2aa" },
+          headerTintColor: "white",
+          headerTitleStyle: { fontWeight: "bold" },
+          //headerTitleAlign: "center",
+        }}
+      >
+        <Homestack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: "Active Alert" }}
+        />
+      </Homestack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ ActiveAlert-App/styles/styles.ts
@@ -0,0 +1,5 @@
+import { StyleSheet } from 'react-native'
+
+export {styles}
+
+const styles = StyleSheet.create({})
